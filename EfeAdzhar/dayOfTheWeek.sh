b4786cfb0d450839fcf900d9fe86af008326a484
EfeAdzhar <efeadzhar@gmail.com>
2022-01-30 23:24:10 +0200

    Fixed Task 3

--- EfeAdzhar/dayOfTheWeek.sh
+++ EfeAdzhar/dayOfTheWeek.sh
@@ -3,13 +3,9 @@ dayOfMonth=$(date +%d)
 dayOfTheWeek=$(date +%u)
 dayOfTheWeekName=$(date +%A)
 
-if (($dayOfTheWeek >= 1 && $dayOfTheWeek <= 5));
+if (($dayOfTheWeek <= 5));
 then
    echo "Look like $dayOfMonth which is $dayOfTheWeekName is a working day"
-
-elif (($dayOfTheWeek = 6)) || (( $dayOfTheWeek = 7));
-then
-   echo "Looks like $dayOfMonth which is $dayOfTheWeekName is a working day"
 else
-   echo "ERROR" 
+   echo "Looks like $dayOfMonth which is $dayOfTheWeekName is a weekend"
 fi
