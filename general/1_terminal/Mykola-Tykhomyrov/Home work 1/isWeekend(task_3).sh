6df97496e57e8092a7f710cf7abef77feb56cadd
lilproger21 <84232684+lilproger21@users.noreply.github.com>
2022-01-31 18:44:42 +0200

    Add new file and remade task_3

--- general/1_terminal/Mykola-Tykhomyrov/Home work 1/isWeekend(task_3).sh
+++ general/1_terminal/Mykola-Tykhomyrov/Home work 1/isWeekend(task_3).sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-today=$(date '+%a')
-if [ $today == 'Сб' ] || [ $today == 'Вс' ] || [ $today == 'Sat' ] || [ $today == 'Sun' ]
-	then echo "looks like $(date +%d) is a weekend day"
-else
-	 echo "looks like $(date +%d) is a working day"
-fi
+[[ `date +%u` -gt 5 ]] && day="weekend" || day="working day"
 
+echo "Looks like $(date +%d) is a $day"
