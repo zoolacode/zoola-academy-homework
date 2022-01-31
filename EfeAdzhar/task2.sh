588df435f426aab2b187205d6109a8a9c2cea05f
Efe Adzhar <efeadzhar@gmail.com>
2022-01-31 16:46:10 +0200

    Update EfeAdzhar/task2.sh
    Co-authored-by: Serhii Lekariev <68739556+sae3023@users.noreply.github.com>

--- EfeAdzhar/task2.sh
+++ EfeAdzhar/task2.sh
@@ -2,7 +2,7 @@
 mkdir -p $1
 cd $1
 
-#ANSWEAR:CD command works, but only inside the script because we launching it in a subprocess.
+#ANSWER:CD command works, but only inside the script because we launching it in a subprocess.
 #Which means, when execution ends, the subprocess will be destroyed, we will be on the current process.
 #To fix the issue, you have to launch it on the current process.
 #Type 'source ./mcd nested/dir' or '. ./mcd nested/dir'.
