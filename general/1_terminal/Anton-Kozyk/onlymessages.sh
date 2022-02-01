c86eefe164ecd763c26f451b88090ced4ab216b4
Anton Kozyk <antonkozik4@gmail.com>
2022-02-01 18:45:16 +0200

    Now messages with links and mentions look like text, not like arrays

--- general/1_terminal/Anton-Kozyk/onlymessages.sh
+++ general/1_terminal/Anton-Kozyk/onlymessages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jq '.messages[].text' result.json
+jq '.messages[].text | if type=="string" then . else [.[] | if type=="object" then .text else . end] | add  end | select(length>0)' $1
