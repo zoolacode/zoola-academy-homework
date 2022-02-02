ac168379d2ebb99127a9c9ce2e0ab0e7ec171855
Leonid <harinlenya1488@gmail.com>
2022-02-02 12:07:13 +0200

    task4

---
+++ general/1_terminal/Leonid-Kharin/task4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash jq
+
+file=$(cat result.json)
+
+echo "please, wait 15 seconds"
+
+# iterate over all keys with value text
+
+for ((i=0; i >(-1); i++)) do 
+
+   text=$(echo $file | jq '.messages'[$i] | jq '.text' ) 
+
+   if [[ $text == "null" ]]; then # when the list ends the value becomes null
+      break
+   fi
+   
+   if [[ $text == *"https"* ]]; then
+      temp=10 
+   else
+       echo $text >> r.json # action that don't matter
+   fi
+
+done
+
+echo $(cat r.json) > result.json 
+rm r.json
