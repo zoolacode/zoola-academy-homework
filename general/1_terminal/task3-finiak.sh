d7fde776804483f7c3391fb630a5b9f2cba9a43a
Marian <finiakmarian@gmail.com>
2022-02-02 13:28:32 +0200

    Changes in task-3

--- general/1_terminal/task3-finiak.sh
+++ general/1_terminal/task3-finiak.sh
@@ -5,10 +5,10 @@ read -p  'enter text: ' name
 day=$(echo $name | awk '{print $NF}')
 NUMBER=$(echo $name | tr -dc '0-9')
 
-if [ $day == 'Sunday' ] || [ $day == 'Saturday' ]
+if [ $day == 'Sunday' ] || [ $day == 'Saturday' ] || [ $day == 'Неділя' ] || [ $day == 'Субота' ] || [ $day == 'Воскресенье' ] || [ $day == 'Суббота' ] || [ $day == 'रविवार' ] || [ $day == 'शनिवार' ]
 then 
 	echo "Looks like $NUMBER is a weekend day" 
-elif [ $day == 'Monday' ] || [ $day == 'Tuesday' ] || [ $day == 'Wednesday' ] || [ $day == 'Thursday' ] || [ $day == 'Friday' ]  
+elif [ $day == 'Monday' ] || [ $day == 'Tuesday' ] || [ $day == 'Wednesday' ] || [ $day == 'Thursday' ] || [ $day == 'Friday' ] || [ $day == 'Понеділок' ] || [ $day == 'Вівторок' ] || [ $day == 'Середа' ] || [ $day == 'Четвер' ] || [ $day == "П'ятниця" ] || [ $day == 'Понедельник' ] || [ $day == 'Вторник' ] || [ $day == 'Среда' ] || [ $day == 'Четверг' ] || [ $day == 'Пятница' ] || [ $day == 'सोमवार' ] || [ $day == 'मंगलवार' ] || [ $day == 'बुधवार' ] || [ $day == 'गुरूवार' ] || [ $day == 'शुक्रवार' ]
 then
 	echo "Looks like $NUMBER is a working day"
 else
