120b227f69e8e2dfa17a843122735b62ad9c486b
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-02-02 12:48:51 +0200

    Fixed errors and remarks

--- general/1_terminal/anatolii-kravchenko/task-1.sh
+++ general/1_terminal/anatolii-kravchenko/task-1.sh
@@ -3,7 +3,7 @@
 # This script is intended for create a bunch of files using touch. 
 # Output the names of those that have zoola in their names.
 
-qtyOfFiles=4                            # Set quantity of files that will be created
+qtyOfFiles=5                            # Set quantity of files that will be created
 
 for (( i = 0; i < $qtyOfFiles; i++ ))   # Loop for creating each file
 do
@@ -11,3 +11,8 @@ do
 done
 
 ls -l | grep Zoola                      # Output files where "Zoola" word is present in the filename
+
+for (( i = 0; i < $qtyOfFiles; i++ ))   # Loop for deleting each created Zoola- file
+do
+    rm "Zoola-$i.txt"
+done
--- general/1_terminal/anatolii-kravchenko/task-3.sh
+++ general/1_terminal/anatolii-kravchenko/task-3.sh
@@ -6,19 +6,8 @@
 dayNumber=$(date +"%d")                                     # Mashine number of the day
 printDayNumber=${dayNumber#"${dayNumber%%[!0]*}"}           # Removed leading zero from Mashine number
 
-case $(date +"%u") in
-    1) echo "Looks like ${printDayNumber} is a working day" # Monday
-        ;;
-    2) echo "Looks like ${printDayNumber} is a working day" # Tuesday
-        ;;
-    3) echo "Looks like ${printDayNumber} is a working day" # Wednesday
-        ;;
-    4) echo "Looks like ${printDayNumber} is a working day" # Thursday
-        ;;
-    5) echo "Looks like ${printDayNumber} is a working day" # Friday
-        ;;
-    6) echo "Looks like ${printDayNumber} is a weekend"     # Saturday
-        ;;
-    7) echo "Looks like ${printDayNumber} is a weekend"     # Sunday
-        ;;
-esac
+if [[ $(date +"%u") -lt 6 ]]; then
+    echo "Looks like ${printDayNumber} is a working day"
+else
+    echo "Looks like ${printDayNumber} is a weekend"
+fi
