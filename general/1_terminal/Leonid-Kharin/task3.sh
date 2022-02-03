45bc50aea078feed572b383e000f8eef7087f3e5
Leonid <harinlenya1488@gmail.com>
2022-02-03 20:35:42 +0200

    correction 4 task version 2

--- general/1_terminal/Leonid-Kharin/task3.sh
+++ general/1_terminal/Leonid-Kharin/task3.sh
@@ -3,9 +3,9 @@
 a=`date +%u`
 a=${a##0} # if number starts with 0 remove 0
 
-if [[ $(date +%u) -ge 6 ]]; then
-        echo "Looks like $a is working day"
+if [[ $(date +%u) -gt 5 ]]; then
+        echo "Looks like $a is weekend day"
 else
-        echo "Looks like $a is a weekend day"
+        echo "Looks like $a is a working day"
 fi
 
