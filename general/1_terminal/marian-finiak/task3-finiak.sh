5bcaf9248e95d22ea81d4d285813f31db0800de5
Marian <finiakmarian@gmail.com>
2022-02-04 14:51:00 +0200

    Chsnges task-3

--- general/1_terminal/marian-finiak/task3-finiak.sh
+++ general/1_terminal/marian-finiak/task3-finiak.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-read -p  'enter text: ' name
-
-NUMBER=$(echo $name | tr -dc '0-9')
-
-if [[ $(date +%u) -gt 5 ]];
-then 
-	echo "Looks like $NUMBER is a weekend day" 
-else
-	echo "Looks like $NUMBER is a working day"
+if [[ $(date +%u) -gt 5 ]]
+  then
+	echo "Looks like $(date +%d) is a weekend"
+  else
+	echo "Looks like $(date +%d) is a working day"
 fi
