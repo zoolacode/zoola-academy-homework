53dba7f2ea47189a197b3926960dff47e11002fe
olnikiforov <nikiforovsh@gmail.com>
2022-07-13 12:12:15 +0300

    [HW1-03] Fractional part calculation done

---
+++ java/Nikiforov-Oleksandr/src/com/zoolatech/lecture1/tasks/_3/Task3.java
@@ -0,0 +1,21 @@
+package com.zoolatech.lecture1.tasks._3;
+import java.util.Scanner;
+
+/**
+ * Write a program that accepts a double number and outputs only its
+ * fractional part (all digits after the decimal point).
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+        // task solution code here
+        Scanner sc = new Scanner(System.in);
+        calculateFractional(sc.nextDouble());
+    }
+
+
+    public static void calculateFractional(double number){
+        //calculate fractional part of number
+        System.out.println(number - (int) number);
+    }
+}
