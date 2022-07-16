b77d6f3eb88e85b247e956d7ac9f75ae2fc54339
Mykola Melnyk <mr.nicholas.x@gmail.com>
2022-07-17 00:04:31 +0300

    task 1.7 done

---
+++ java/mykola-melnyk/src/com/zoolatech/lecture1/tasks/_7/Main.java
@@ -0,0 +1,27 @@
+package com.zoolatech.lecture1.tasks._7;
+
+/**
+ * Write a program that prints all odd numbers from N to 1.
+ * N should be read from the input. Every number should be printed on the new line.
+ * Create two versions of the program: one should use the for loop, second - the while loop.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input an integer:");
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Converting your even number to odd...");
+            int oddnum = num - 1;
+            for (int i = oddnum; i > 0; i -= 2)
+                System.out.println(i);
+        }
+        else {
+            for (int i = num; i > 0; i -= 2)
+                System.out.println(i);
+        }
+    }
+}
