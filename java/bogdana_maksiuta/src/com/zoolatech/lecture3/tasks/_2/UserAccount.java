96ef6418f18bb742c7a8472f769a621ceea5a536
Dana <danetqt@gmail.com>
2022-07-22 00:19:56 +0300

    [HW03-02] Add implementation for user account validator with subvalidators for each field

---
+++ java/bogdana_maksiuta/src/com/zoolatech/lecture3/tasks/_2/UserAccount.java
@@ -0,0 +1,58 @@
+package com.zoolatech.lecture3.tasks._2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+public class UserAccount {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String userEmail;
+    private String phoneNumber;
+    private int birthDay;
+    private int birthMonth;
+
+    public UserAccount(int userId, String firstName, String lastName, String country,
+                       String userEmail, String phoneNumber, int birthDay, int birthMonth) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.userEmail = userEmail;
+        this.phoneNumber = phoneNumber;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserAccount that = (UserAccount) o;
+        return userId == that.userId && birthDay == that.birthDay && birthMonth == that.birthMonth && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(country, that.country) && Objects.equals(userEmail, that.userEmail) && Objects.equals(phoneNumber, that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, firstName, lastName, country, userEmail, phoneNumber, birthDay, birthMonth);
+    }
+
+    @Override
+    public String toString() {
+        return "UserAccount{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", birthDay=" + birthDay +
+                ", birthMonth=" + birthMonth +
+                '}';
+    }
+}
---
+++ java/bogdana_maksiuta/src/com/zoolatech/lecture3/tasks/_2/Validator.java
@@ -0,0 +1,7 @@
+package com.zoolatech.lecture3.tasks._2;
+
+interface Validator {
+    boolean isValid(UserAccount userAccount);
+
+}
+
