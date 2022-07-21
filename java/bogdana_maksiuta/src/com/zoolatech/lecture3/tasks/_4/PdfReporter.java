c69158deb85fb94940835f36e53e978258488b03
Dana <danetqt@gmail.com>
2022-07-22 00:35:26 +0300

    [HW03-04] Implement logic for creating files with different formats

---
+++ java/bogdana_maksiuta/src/com/zoolatech/lecture3/tasks/_4/PdfReporter.java
@@ -0,0 +1,12 @@
+package com.zoolatech.lecture3.tasks._4;
+
+public class PdfReporter extends Reporter {
+
+    public PdfReporter(String headerData, String fileData, String fileName) {
+        super(headerData, fileData, fileName);
+    }
+
+    void generate() {
+        super.generate();
+    }
+}
