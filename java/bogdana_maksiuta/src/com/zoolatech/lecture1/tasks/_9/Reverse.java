f2b705434acccc42a9962e7c8f76a3af37f481b4
Dana <danetqt@gmail.com>
2022-07-21 23:13:23 +0300

    [HW01-9] Add logic to accept number and display digits in the reverse order

---
+++ java/bogdana_maksiuta/src/com/zoolatech/lecture1/tasks/_9/Reverse.java
@@ -0,0 +1,45 @@
+package com.zoolatech.lecture1.tasks._9;
+
+import java.util.Scanner;
+
+/**
+ * 9. Write a program that accepts an integer value and outputs all its digits in the reverse order on a new line.
+ a. Input: 12345
+ *      Output: 5
+ *              4
+ *              3
+ *              2
+ *              1
+ */
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        reverseNumberWhileLoop(number);
+        reverseNumberForLoop (number);
+    }
+
+    public static void reverseNumberWhileLoop (int number) {
+        System.out.println("While loop: ");
+        int numberForReverse = 0;
+        while (number != 0) {
+            int lastNumber = number % 10;
+            numberForReverse = numberForReverse * 10 + lastNumber;
+            number = number/10;
+            System.out.println(lastNumber);
+        }
+    }
+
+    public static void reverseNumberForLoop (int number) {
+        System.out.println("For loop: ");
+        int numberForReverse = 0;
+        for (int i = number; i != 0 ; i /= 10) {
+            int lastNumber = i % 10;
+            System.out.println(lastNumber);
+            numberForReverse = numberForReverse * 10 + lastNumber;
+        }
+    }
+}
+
