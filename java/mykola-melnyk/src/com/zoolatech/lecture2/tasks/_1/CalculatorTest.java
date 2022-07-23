86a733d8f5f99975ef8374844b2c649649fd65c9
Mykola-Melnyk <mr.nicholas.x@gmail.com>
2022-07-23 19:33:01 +0300

    Lecture 2 task 1: Resolved: Added patch to ignore division by 0 without modification - now showing a warning message instead.

--- java/mykola-melnyk/src/com/zoolatech/lecture2/tasks/_1/CalculatorTest.java
+++ java/mykola-melnyk/src/com/zoolatech/lecture2/tasks/_1/CalculatorTest.java
@@ -4,6 +4,6 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calc = new Calculator(3);
         calc.divide(0);
-        System.out.println(calc.getStoredValue());
+//        System.out.println(calc.getStoredValue());
     }
 }
