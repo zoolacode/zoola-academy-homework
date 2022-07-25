aac85a4dcb9c63ee7f48bcf8bfb6792c251145ff
olnikiforov <nikiforovsh@gmail.com>
2022-07-25 13:23:58 +0300

    [HW03-1] Orchestra task done

---
+++ java/Nikiforov-Oleksandr/src/com/zoolatech/lecture3/tasks/_1/Cello.java
@@ -0,0 +1,12 @@
+package com.zoolatech.lecture3.tasks._1;
+
+public class Cello extends MusicalInstrument {
+    public Cello(String sound, String name) {
+        super(sound, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Cello";
+    }
+}
---
+++ java/Nikiforov-Oleksandr/src/com/zoolatech/lecture3/tasks/_1/Piano.java
@@ -0,0 +1,14 @@
+package com.zoolatech.lecture3.tasks._1;
+
+public class Piano extends MusicalInstrument {
+
+    public Piano(String sound, String name) {
+        super(sound, name);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Piano";
+    }
+}
