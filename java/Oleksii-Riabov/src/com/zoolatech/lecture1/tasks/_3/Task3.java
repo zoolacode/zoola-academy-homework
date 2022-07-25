dd6b751843b5a9a35094185707a3d8acf17e0815
Oleksii Riabov <ryaboff99@gmail.com>
2022-07-25 23:56:44 +0300

    [HW-1]: Update Task3
    * remove extra new line after main method start.

--- java/Oleksii-Riabov/src/com/zoolatech/lecture1/tasks/_3/Task3.java
+++ java/Oleksii-Riabov/src/com/zoolatech/lecture1/tasks/_3/Task3.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Task3 {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         if(scanner.hasNextFloat()){
