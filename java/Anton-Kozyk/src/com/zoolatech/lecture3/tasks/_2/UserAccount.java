5c78cf86953f1ef79f1958f31a19a7ef297db746
AntonKun <antonkozik4@gmail.com>
2022-07-27 22:17:05 +0300

    [HW3] Change the type of UserAccount from class to record

--- java/Anton-Kozyk/src/com/zoolatech/lecture3/tasks/_2/UserAccount.java
+++ java/Anton-Kozyk/src/com/zoolatech/lecture3/tasks/_2/UserAccount.java
@@ -1,24 +1,4 @@
 package com.zoolatech.lecture3.tasks._2;
 
-public class UserAccount {
-    int userId;
-    String firstName;
-    String lastName;
-    String country;
-    String userEmail;
-    String phoneNumber;
-    int birthDay;
-    int birthMonth;
-
-    UserAccount(int userId, String firstName, String lastName, String country, String userEmail,
-                String phoneNumber, int birthDay, int birthMonth) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.country = country;
-        this.userEmail = userEmail;
-        this.phoneNumber = phoneNumber;
-        this.birthDay = birthDay;
-        this.birthMonth = birthMonth;
-    }
-}
+public record UserAccount(int userId, String firstName, String lastName, String country,
+                          String userEmail, String phoneNumber, int birthDay, int birthMonth) {}
