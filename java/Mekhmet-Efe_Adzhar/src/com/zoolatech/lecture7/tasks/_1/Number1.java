ad1ccaaa72a3821cc57cb0ba407f12095514552f
EfeAdzhar <efeadzhar@gmail.com>
2022-08-04 19:12:22 +0200

    [HW-7]: Resolved conflicts.

--- java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture7/tasks/_1/Number1.java
+++ java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture7/tasks/_1/Number1.java
@@ -1,4 +1,7 @@
 package com.zoolatech.lecture7.tasks._1;
 
 public class Number1 {
+    public static void main(String[] args) {
+
+    }
 }
--- java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture7/tasks/_2/Number2.java
+++ java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture7/tasks/_2/Number2.java
@@ -1,4 +1,7 @@
 package com.zoolatech.lecture7.tasks._2;
 
 public class Number2 {
+    public static void main(String[] args) {
+
+    }
 }
