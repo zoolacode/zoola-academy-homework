6e3f508d41b103d3251c063e5f675d97f1c99eaf
Mykola-Melnyk <mr.nicholas.x@gmail.com>
2022-08-05 14:39:59 +0300

    Lecture 4: Task 3

---
+++ java/mykola-melnyk/src/com/zoolatech/lecture4/tasks/_2/BinarySearchTree.java
@@ -0,0 +1,16 @@
+package com.zoolatech.lecture4.tasks._2;
+
+public class BinarySearchTree {
+    private Node rootNode;
+    static class Node {
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
+        public Node (int value) {
+            this.value = value;
+        }
+
+
+
+    }
+}
---
+++ java/mykola-melnyk/src/com/zoolatech/lecture4/tasks/_2/Task2.java
@@ -0,0 +1,12 @@
+package com.zoolatech.lecture4.tasks._2;
+
+/**
+ * Create a class that represents a binary search tree (https://en.wikipedia.org/wiki/Binary_search_tree).
+ * The tree should have a reference to the root node. Each node stores an integer value and has references
+ * to the left and right children nodes. The tree should also have a method that accepts a value and adds
+ * it to the tree.
+ */
+
+public class Task2 {
+
+}
