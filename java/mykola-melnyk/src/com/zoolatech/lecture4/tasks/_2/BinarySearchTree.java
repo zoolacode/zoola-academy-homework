d660747cd7391cff61a1c334e3152df4d6bb46e9
Mykola-Melnyk <mr.nicholas.x@gmail.com>
2022-08-06 16:37:07 +0300

    Lecture 4: Task 2 done

--- java/mykola-melnyk/src/com/zoolatech/lecture4/tasks/_2/BinarySearchTree.java
+++ java/mykola-melnyk/src/com/zoolatech/lecture4/tasks/_2/BinarySearchTree.java
@@ -2,15 +2,37 @@ package com.zoolatech.lecture4.tasks._2;
 
 public class BinarySearchTree {
     private Node rootNode;
+    public BinarySearchTree () {
+        rootNode = null;
+    }
+
     static class Node {
         private int value;
         private Node leftChild;
         private Node rightChild;
         public Node (int value) {
             this.value = value;
+            leftChild = null;
+            rightChild = null;
+        }
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+        if (value < current.value) {
+            current.leftChild = addRecursive(current.leftChild, value);
+        } else if (value > current.value) {
+            current.rightChild = addRecursive(current.rightChild, value);
+        } else {
+            return current;
         }
 
-
-
+        return current;
     }
+    public void add(int value) {
+        rootNode = addRecursive(rootNode, value);
+    }
+
 }
--- java/mykola-melnyk/src/com/zoolatech/lecture4/tasks/_2/Task2.java
+++ java/mykola-melnyk/src/com/zoolatech/lecture4/tasks/_2/Task2.java
@@ -8,5 +8,12 @@ package com.zoolatech.lecture4.tasks._2;
  */
 
 public class Task2 {
-
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.add(10);
+        bst.add(5);
+        bst.add(20);
+        bst.add(40);
+        bst.add(60);
+    }
 }
