518345954c82e8a124926e87fca25ba84d614cf9
Oleksii Riabov <ryaboff99@gmail.com>
2022-08-14 15:50:18 +0300

    [HW-4]: Update Task2
    * Define Node as a separate class
    * Remove redundant null assignments from Node constructor
    * Make "root" variable of Tree class private
    * Change order of methods in Tree.java for better readability
    * Remove redundant "else" statement from "addRecursive" method of Tree.java

--- java/Oleksii-Riabov/src/com/zoolatech/lecture4/tasks/_2/Tree.java
+++ java/Oleksii-Riabov/src/com/zoolatech/lecture4/tasks/_2/Tree.java
@@ -2,19 +2,11 @@ package com.zoolatech.lecture4.tasks._2;
 
 public class Tree {
 
-    class Node {
-    int value;
-    Node leftNode;
-    Node rightNode;
+    private Node root;
 
-    Node(int value) {
-        this.value = value;
-        rightNode = null;
-        leftNode = null;
+    public void add(int value) {
+        root = addRecursive(root, value);
     }
-}
-
-    Node root;
 
     private Node addRecursive(Node current, int value) {
         if (current == null) {
@@ -25,15 +17,9 @@ public class Tree {
             current.leftNode = addRecursive(current.leftNode, value);
         } else if (value > current.value) {
             current.rightNode = addRecursive(current.rightNode, value);
-        } else {
-            return current;
         }
 
         return current;
-    }
-
-    public void add(int value) {
-        root = addRecursive(root, value);
     }
 
     void inorder() {
