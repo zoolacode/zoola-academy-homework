3226d6f73225f9f50f162a894f84358d506083ed
Oleksii Riabov <ryaboff99@gmail.com>
2022-08-14 13:58:58 +0300

    [HW-3]: Update Task3
    * Remove redundant modifiers rom interface Shape.java

--- java/Oleksii-Riabov/src/com/zoolatech/lecture3/tasks/_3/Shape.java
+++ java/Oleksii-Riabov/src/com/zoolatech/lecture3/tasks/_3/Shape.java
@@ -1,7 +1,7 @@
 package com.zoolatech.lecture3.tasks._3;
 
 public sealed interface Shape permits Circle, Triangle, Rectangle {
-    public abstract void findPerimeter();
+    void findPerimeter();
 
-    public abstract void findArea();
+    void findArea();
 }
