22707bd980f41cdcabb67019aa8754c403f0258f
Oleksii Riabov <ryaboff99@gmail.com>
2022-08-15 22:43:35 +0300

    [HW-6]: Update Task1
    * Replace wildcard import with explicit imports

--- java/Oleksii-Riabov/src/com/zoolatech/lecture6/tasks/_1/Task1.java
+++ java/Oleksii-Riabov/src/com/zoolatech/lecture6/tasks/_1/Task1.java
@@ -1,6 +1,9 @@
 package com.zoolatech.lecture6.tasks._1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
