b169edc9712f708a35751d8885442f3a20cc9b88
Oleg Cherepnin <cherepnin11@gmail.com>
2022-08-16 11:08:04 +0300

    clean-up records: remove extra code

--- java/Cherepnin_Oleh/src/com/zoolatech/lecture6/tasks/_1/City.java
+++ java/Cherepnin_Oleh/src/com/zoolatech/lecture6/tasks/_1/City.java
@@ -1,30 +1,4 @@
 package com.zoolatech.lecture6.tasks._1;
 
-import java.util.Objects;
-
 public record City(String name, String country, int population) {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof City)) return false;
-        City city = (City) o;
-        return population == city.population
-                && name.equals(city.name)
-                && country.equals(city.country);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, country, population);
-    }
-
-    @Override
-    public String toString() {
-        return "City{" +
-                "name='" + name + '\'' +
-                ", country='" + country + '\'' +
-                ", population=" + population +
-                '}';
-    }
 }
--- java/Cherepnin_Oleh/src/com/zoolatech/lecture6/tasks/_2/Order.java
+++ java/Cherepnin_Oleh/src/com/zoolatech/lecture6/tasks/_2/Order.java
@@ -9,27 +9,4 @@ public record Order(String id, StoreType storeType, float price, String country)
         this(String.valueOf(idCounter), type, price, country);
         idCounter++;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Order order)) return false;
-        return Float.compare(order.price, price) == 0
-                && Objects.equals(id, order.id)
-                && storeType == order.storeType
-                && Objects.equals(country, order.country);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, storeType, price, country);
-    }
-
-    @Override
-    public String toString() {
-        return "order#" + id + "(\"" + id +
-                "\", \"" + storeType +
-                "\", " + price +
-                ", \"" + country + "\")";
-    }
 }
