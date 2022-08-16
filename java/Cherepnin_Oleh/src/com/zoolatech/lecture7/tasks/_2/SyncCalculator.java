c906b609db4e4dc048ef7ef45b045a65be4a15a9
Oleg Cherepnin <cherepnin11@gmail.com>
2022-08-16 11:14:20 +0300

    solve comments

--- java/Cherepnin_Oleh/src/com/zoolatech/lecture7/tasks/_2/SyncCalculator.java
+++ java/Cherepnin_Oleh/src/com/zoolatech/lecture7/tasks/_2/SyncCalculator.java
@@ -1,25 +1,19 @@
 package com.zoolatech.lecture7.tasks._2;
 
 public class SyncCalculator implements Calculator {
-    private volatile int value;
+    private int value;
 
-    public void add(int addedValue) {
-        synchronized (this) {
-            value += addedValue;
-            System.out.println("add: " + addedValue);
-        }
+    public synchronized void add(int addedValue) {
+        value += addedValue;
+        System.out.println("add: " + addedValue);
     }
 
-    public void subtract(int subtracted) {
-        synchronized (this) {
-            value -= subtracted;
-            System.out.println("subtract: " + subtracted);
-        }
+    public synchronized void subtract(int subtracted) {
+        value -= subtracted;
+        System.out.println("subtract: " + subtracted);
     }
 
-    public int getValue() {
-        synchronized (this) {
-            return value;
-        }
+    public synchronized int getValue() {
+        return value;
     }
 }
