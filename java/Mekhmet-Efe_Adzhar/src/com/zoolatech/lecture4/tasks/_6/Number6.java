2fdd3b63cec8914e0764665ed25021c61b1ce60e
EfeAdzhar <efeadzhar@gmail.com>
2022-08-17 22:30:00 +0200

    [FIXES-2][HW-4]: Task 6. Fixes done

--- java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture4/tasks/_6/Number6.java
+++ java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture4/tasks/_6/Number6.java
@@ -14,70 +14,17 @@ Output:
 public class Number6 {
     public static void main(String[] args) {
         String string = "aB?C de 42* dcd";
-        VariationOne variationOne = new VariationOne();
-        variationOne.countStringSymbolsVariationOne(string);
-        VariationTwo variationTwo = new VariationTwo();
-        variationTwo.countStringSymbolsVariationTwo(string);
+        CharCounter charCounter = new CharCounter();
+        charCounter.countStringSymbolsVariationTwo(string);
     }
 }
 
-class VariationOne {
-
-    public void countStringSymbolsVariationOne(String string) {
-        int counterOfChars = 0;
-        int counterOfDigits = 0;
-        int couterForSpaces = 0;
-        int counterForOtherCharacters = 0;
-
-        for (char c = 'a'; c <= 'z'; c++) {
-            for (int i = 0; i <= string.length() - 1; i++) {
-                if (c == string.charAt(i)) {
-                    counterOfChars++;
-                }
-            }
-        }
-        for (char c = 'A'; c <= 'Z'; c++) {
-            for (int i = 0; i <= string.length() - 1; i++) {
-                if (c == string.charAt(i)) {
-                    counterOfChars++;
-                }
-            }
-        }
-        System.out.print(counterOfChars + " " + "letters");
-        System.out.println();
-
-        char[] chars = string.toCharArray();
-        for (char c : chars) {
-            if (Character.isDigit(c)) {
-                counterOfDigits++;
-            }
-        }
-        System.out.print(counterOfDigits + " " + "digits");
-        System.out.println();
-
-        for (char c : chars) {
-            if (c == ' ') {
-                couterForSpaces++;
-            }
-        }
-        System.out.print(couterForSpaces + " " + "spaces");
-        System.out.println();
-
-        for (char c : chars) {
-            if (!Character.isDigit(c) && !Character.isLetter(c) && !Character.isSpaceChar(c)) {
-                counterForOtherCharacters++;
-            }
-        }
-        System.out.print(counterForOtherCharacters + " " + "other characters");
-    }
-}
-
-class VariationTwo {
+class CharCounter {
 
     public void countStringSymbolsVariationTwo(String string) {
         int counterOfChars = 0;
         int counterOfDigits = 0;
-        int couterForSpaces = 0;
+        int counterForSpaces = 0;
         int counterForOtherCharacters = 0;
 
         char[] chars = string.toCharArray();
@@ -88,14 +35,14 @@ class VariationTwo {
             } else if (Character.isDigit(c)) {
                 counterOfDigits++;
             } else if (Character.isSpaceChar(c)) {
-                couterForSpaces++;
-            } else if (!Character.isDigit(c) && !Character.isLetter(c) && !Character.isSpaceChar(c)) {
+                counterForSpaces++;
+            } else  {
                 counterForOtherCharacters++;
             }
         }
         System.out.println(counterOfChars + " " + "letters");
         System.out.println(counterOfDigits + " " + "digits");
-        System.out.println(couterForSpaces + " " + "spaces");
+        System.out.println(counterForSpaces + " " + "spaces");
         System.out.println(counterForOtherCharacters + " " + "other characters");
     }
 }
