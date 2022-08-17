e58e77430f7f5520dc0d0c71824230b0e864f9d0
EfeAdzhar <efeadzhar@gmail.com>
2022-08-17 22:25:45 +0200

    [FIXES-2][HW-4]: Task 5. Fixes done

--- java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture4/tasks/_5/Number5.java
+++ java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture4/tasks/_5/Number5.java
@@ -7,54 +7,35 @@ The program should work with decimal numbers of any length and precision.
  */
 
 import java.math.BigDecimal;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Number5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input first number, operation and second number");
-        Calculation calculation = new Calculation(scanner.nextBigDecimal(), scanner.next().charAt(0), scanner.nextBigDecimal());
-        System.out.println(calculation.calculations());
-    }
-}
-
-class Calculation {
-    BigDecimal numberOne;
-    char operation;
-    BigDecimal numberTwo;
-
-    public Calculation(BigDecimal numberOne, char operation, BigDecimal numberTwo) {
-        this.numberOne = numberOne;
-        this.operation = operation;
-        this.numberTwo = numberTwo;
+        System.out.println(calculations(scanner.nextBigDecimal(), scanner.next().charAt(0), scanner.nextBigDecimal()));
     }
 
-    public BigDecimal calculations() {
-        BigDecimal bigDecimal = null;
-        try {
-            switch (operation) {
-                case '+':
-                    bigDecimal = numberOne.add(numberTwo);
-                    return bigDecimal;
-                case '-':
-                    bigDecimal = numberOne.subtract(numberTwo);
-                    return bigDecimal;
-                case '*':
-                    bigDecimal = numberOne.multiply(numberTwo);
-                    return bigDecimal;
-                case '/':
-                    bigDecimal = numberOne.divide(numberTwo);
-                    return bigDecimal;
-                case '%':
-                    bigDecimal = numberOne.remainder(numberTwo);
-                    return bigDecimal;
-                default:
-                    return bigDecimal;
+    public static BigDecimal calculations(BigDecimal numberOne, char operation, BigDecimal numberTwo) {
+        switch (operation) {
+            case '+' -> {
+                return numberOne.add(numberTwo);
             }
-        } catch (InputMismatchException error) {
-            System.out.println("False input" + error);
+            case '-' -> {
+                return numberOne.subtract(numberTwo);
+            }
+            case '*' -> {
+                return numberOne.multiply(numberTwo);
+            }
+            case '/' -> {
+                return numberOne.divide(numberTwo);
+            }
+            case '%' -> {
+                return numberOne.remainder(numberTwo);
+            }
+            default -> {
+                return null;
+            }
         }
-        return bigDecimal;
     }
 }
