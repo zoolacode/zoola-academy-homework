15d4983dd30587c54a707252277bbbdb5cfa1329
olnikiforov <nikiforovsh@gmail.com>
2022-08-18 11:22:22 +0300

    [HW8-02] Image processor task done

---
+++ java/Nikiforov-Oleksandr/src/com/zoolatech/lecture8/tasks/_2/Task2.java
@@ -0,0 +1,23 @@
+package com.zoolatech.lecture8.tasks._2;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * Create a class ImageProcessorTask that accepts an image (represented as an int[][] array, where each item of
+ * an array is an individual pixel) and applies some transformation to each pixel (your choice, e.g.,
+ * multiplies it by 10). The class needs to recursively split the processing task into smaller subtasks (subimages),
+ * and start processing of a subimage only if its width and height are <= 10 pixels. Note: you don’t need to create
+ * a new subarray for each subtask, it’s enough to pass coordinates of its bounds. Each subtask should be processed
+ * concurrently. Hint: the simplest way would be to split an image on each step to 4 quadrants.
+ * Imagine that an initial image is just a big quadrant.
+ */
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[][] pixels = new int[60][80];
+        ImageProcessorTasks imageTask = new ImageProcessorTasks(pixels);
+        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+        forkJoinPool.invoke(imageTask);
+    }
+}
