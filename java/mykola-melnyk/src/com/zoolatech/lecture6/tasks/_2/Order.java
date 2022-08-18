f7f6cb71e6916aa9847d41abb378ba4b110688d0
Mykola-Melnyk <mr.nicholas.x@gmail.com>
2022-08-18 18:56:38 +0300

    Lecture 6 task 2

--- java/mykola-melnyk/src/com/zoolatech/lecture6/tasks/_2/Order.java
+++ java/mykola-melnyk/src/com/zoolatech/lecture6/tasks/_2/Order.java
@@ -1,5 +1,7 @@
 package com.zoolatech.lecture6.tasks._2;
 
+import java.util.Objects;
+
 enum Type {
     WEBSITE,
     STORE
@@ -15,6 +17,19 @@ public class Order {
         this.orderType = orderType;
         this.price = price;
         this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id.equals(order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     public String getId() {
