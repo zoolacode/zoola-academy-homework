5ee83e6534e50128f40adfd45cc861451c07727e
EfeAdzhar <efeadzhar@gmail.com>
2022-08-19 23:00:35 +0200

    [FIXES-2][HW-5]: Task 1. Fixes done

--- java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture5/tasks/_1/Number1.java
+++ java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture5/tasks/_1/Number1.java
@@ -10,75 +10,29 @@ Input: 3, 1, 2
 Output: -1
 */
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Number1 {
+    ArrayList<Integer> arrayList = new ArrayList<>();
+
     public static void main(String[] args) {
-
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(5);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(7);
-
-        int N = 4;
-
+        int n = 4;
         Number1 number1 = new Number1();
-        number1.task(N, arrayList);
+        number1.task(n, 1, 5, 2, 3, 7);
     }
 
-    public void task(int N, ArrayList<Integer> arrayList) {
-        ArrayList<Integer> arrayListOriginal = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            arrayListOriginal.add(i);
-        }
-        Collections.copy(arrayListOriginal, arrayList);
-
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (N - 1 > arrayList.size()) {
-                System.out.println(error(N, arrayList));
-                break;
-            } else if (N - 1 < arrayList.size()) {
-                ArrayListEditor.ArrayListSort arrayListSort = new ArrayListEditor.ArrayListSort();
-                arrayListSort.sortArrayList(arrayList);
-
-                if (arrayList.get(i) == arrayList.toArray()[N - 1]) {
-                    System.out.print(arrayList.get(i) + " the " + N + "th smallest number in the array " + arrayListOriginal + " is ");
-                    ArrayListEditor arrayListSorted = new ArrayListEditor(arrayList);
-                    System.out.print(arrayList.get(i) + "" + arrayListSorted);
-                }
+    public void task(int n, int... variables) {
+        if (n > variables.length + 1) {
+            System.out.println(-1);
+        } else {
+            for (int variable : variables) {
+                arrayList.add(variable);
             }
         }
-    }
-
-    public int error(int N, ArrayList<Integer> arrayList) {
-
-        for (int ignored : arrayList) {
-            if (N > arrayList.size()) {
-                return -1;
-            }
-        }
-        return 0;
-    }
-}
-
-class ArrayListEditor {
-
-    ArrayList<Integer> arrayList;
-
-    public ArrayListEditor(ArrayList<Integer> arrayList) {
-        this.arrayList = arrayList;
-    }
-
-    static class ArrayListSort {
-        public void sortArrayList(ArrayList<Integer> arrayList) {
-            Collections.sort(arrayList);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return ": " + arrayList;
+        Collections.sort(arrayList);
+        System.out.print(arrayList.get(n - 1) + " (the " + n + "th smallest number in the array ");
+        System.out.println(Arrays.toString(variables) + " is " + arrayList.get(n - 1) + ": " + arrayList);
     }
 }
