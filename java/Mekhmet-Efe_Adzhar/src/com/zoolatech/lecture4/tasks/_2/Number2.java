428b650c5141302a9ffffa457dfff29125e6cd5f
EfeAdzhar <efeadzhar@gmail.com>
2022-08-23 14:19:06 +0200

    [FIXES-3][HW-4]: Task 2. Fixes done

--- java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture4/tasks/_2/Number2.java
+++ java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture4/tasks/_2/Number2.java
@@ -9,97 +9,89 @@ The tree should also have a method that accepts a value and adds it to the tree.
 
 public class Number2 {
     public static void main(String[] args) {
-        Node node = new Node(12);
-        BinaryTree binaryTree = new BinaryTree(node);
-        binaryTree.insert(node, 15);
-        binaryTree.insert(node, 10);
-        binaryTree.insert(node, 20);
-        binaryTree.insert(node, 1);
-        binaryTree.insert(node, 13);
-        binaryTree.delete(node, 13);
-        binaryTree.delete(node, 10);
-        binaryTree.order(node);
-        System.out.println("ROOT NODE: " + binaryTree.getRootNodeValue());
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.insert(12);
+        binaryTree.insert(15);
+        binaryTree.insert(10);
+        binaryTree.insert(20);
+        binaryTree.insert(1);
+        binaryTree.insert(13);
+        binaryTree.delete(13);
+        binaryTree.delete(10);
+        binaryTree.order();
+        System.out.println("Root node: " + binaryTree.getRootNodeValue());
     }
 }
 
-record BinaryTree(Node rootNode) {
+class BinaryTree {
+   private Node rootNode;
 
-    public int getRootNodeValue() {
-        return rootNode.getValue();
+    private static class Node {
+        int value;
+        Node leftChild;
+        Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
     }
 
-    public Node insert(Node node, int value) {
+    public int getRootNodeValue() {
+        return rootNode.value;
+    }
+
+    public void insert(int value) {
+        rootNode = insertNode(rootNode, value);
+    }
+
+    public void delete(int value) {
+        rootNode = deleteNode(rootNode, value);
+    }
+
+    public void order() {
+      orderNode(rootNode);
+    }
+
+    private Node insertNode(Node node, int value) {
         if (node == null) {
             node = new Node(value);
-        } else if (value <= node.getValue()) {
-            node.setLeftChild(insert(node.getLeftChild(), value));
+        } else if (value <= node.value) {
+            node.leftChild = insertNode(node.leftChild, value);
         } else {
-            node.setRightChild(insert(node.getRightChild(), value));
+            node.rightChild = insertNode(node.rightChild, value);
         }
         return node;
     }
 
-    public void order(Node node) {
+    private void orderNode(Node node) {
         if (node != null) {
-            order(node.getLeftChild());
-            System.out.println(node.getValue());
-            order(node.getRightChild());
+            orderNode(node.leftChild);
+            System.out.println(node.value);
+            orderNode(node.rightChild);
         }
     }
 
-    public Node delete(Node node, int value) {
-        if (value < node.getValue()) {
-            node.setLeftChild(delete(node.getLeftChild(), value));
-        } else if (value > node.getValue()) {
-            node.setRightChild(delete(node.getRightChild(), value));
+    private Node deleteNode(Node node, int value) {
+        if (node == null) {
+            node = new Node(value);
+        }
+        if (value < node.value) {
+            node.leftChild = deleteNode(node.leftChild, value);
+        } else if (value > node.value) {
+            node.rightChild = deleteNode(node.rightChild, value);
         } else {
-            if (node.getRightChild() == null && node.getLeftChild() == null) {
+            if (node.rightChild == null && node.leftChild == null) {
                 node = null;
-            } else if (node.getRightChild() == null) {
-                node = node.getLeftChild();
-            } else if (node.getLeftChild() == null) {
-                node = node.getRightChild();
+            } else if (node.rightChild == null) {
+                node = node.leftChild;
+            } else if (node.leftChild == null) {
+                node = node.rightChild;
             } else {
-                Node temp = node.getRightChild();
-                node.setValue(temp.getValue());
-                node.setRightChild(delete(node.getRightChild(), temp.getValue()));
+                Node temp = node.rightChild;
+                node.value = temp.value;
+                node.rightChild = deleteNode(node.rightChild, temp.value);
             }
         }
         return node;
-    }
-}
-
-class Node {
-    private int value;
-    private Node leftChild;
-    private Node rightChild;
-
-    public Node(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public Node getLeftChild() {
-        return leftChild;
-    }
-
-    public Node getRightChild() {
-        return rightChild;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public void setLeftChild(Node leftChild) {
-        this.leftChild = leftChild;
-    }
-
-    public void setRightChild(Node rightChild) {
-        this.rightChild = rightChild;
     }
 }
