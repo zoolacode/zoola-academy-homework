18f05359b3dd2ccb7debb345ec668554ccf1629d
EfeAdzhar <efeadzhar@gmail.com>
2022-08-24 21:44:23 +0200

    [FIXES-2][HW-5]: Task 3. Fixes done

--- java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture5/tasks/_3/Number3.java
+++ java/Mekhmet-Efe_Adzhar/src/com/zoolatech/lecture5/tasks/_3/Number3.java
@@ -12,35 +12,30 @@ Output: apple
  */
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Number3 {
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<>();
-        stringList.add("Apple");
-        stringList.add("orange");
-        stringList.add("pear");
-        stringList.add("apple");
-        stringList.add("banana");
-        stringList.add("orange");
+        ArrayList<String> fruitList = new ArrayList<>();
+        fruitList.add("Apple");
+        fruitList.add("orange");
+        fruitList.add("pear");
+        fruitList.add("apple");
+        fruitList.add("banana");
+        fruitList.add("orange");
 
         Number3 number3 = new Number3();
-        System.out.println(number3.uniqueWords(stringList));
+        System.out.println(number3.uniqueWords(fruitList));
     }
 
-    public List<String> uniqueWords(List<String> stringList) {
-        for (int i = 0; i < stringList.size(); i++) {
-            for (int j = i + 1; j < stringList.size(); j++) {
-                if (stringList.get(i).equals(stringList.get(j))) {
-                    stringList.remove(j);
-                    j--;
-                }
-                if (stringList.get(i).equalsIgnoreCase(stringList.get(j))) {
-                    stringList.remove(j);
+    public ArrayList<String> uniqueWords(ArrayList<String> fruitList) {
+        for (int i = 0; i < fruitList.size(); i++) {
+            for (int j = i + 1; j < fruitList.size(); j++) {
+                if (fruitList.get(i).equalsIgnoreCase(fruitList.get(j))) {
+                    fruitList.remove(j);
                     j--;
                 }
             }
         }
-        return stringList;
+        return fruitList;
     }
 }
