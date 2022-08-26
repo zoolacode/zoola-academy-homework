d80049e970072d608c3de016a1f70a6f2da9e67f
Mykola Melnyk <mr.nicholas.x@gmail.com>
2022-08-26 22:06:00 +0300

    out folder added, 7.1 done

--- java/mykola-melnyk/src/com/zoolatech/lecture7/tasks/_1/Main.java
+++ java/mykola-melnyk/src/com/zoolatech/lecture7/tasks/_1/Main.java
@@ -3,11 +3,14 @@ package com.zoolatech.lecture7.tasks._1;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Runnable r = () -> {
-            System.out.println(Stream.generate(Math::random).limit(10).toList());
+            while (!Thread.currentThread().isInterrupted())
+                System.out.println((int) (Math.random() * 10));
         };
         var t = new Thread(r);
         t.start();
+        Thread.sleep(100);
+        t.interrupt();
     }
 }
