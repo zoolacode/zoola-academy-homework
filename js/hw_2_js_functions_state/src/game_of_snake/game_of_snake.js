d4d950f3b55fb85b1ec70f52d815580cab497d3d
Anna <horlacheva.ann@gmail.com>
2022-09-03 01:21:47 +0300

    Build game GameSnake

--- js/hw_2_js_functions_state/src/game_of_snake/game_of_snake.js
+++ js/hw_2_js_functions_state/src/game_of_snake/game_of_snake.js
@@ -1,24 +1,24 @@
 import { createEngine, ENGINE_INITIALIZE_SIGNAL } from "../engine/engine";
 
-const meshSize = 625;
+const boardWidth = 25;
+const meshSize = boardWidth ** 2;
 
 const initialState = {
-    currentScore: 0,
-    endGameWinning: false,
-    endGameLosing: false,
-    isGameRestart: false,
-    pause: false,
-    time: 0,
-    snakeBody: [
-      [10, 10],
-      [10, 11],
-      [10, 12],
-      [10, 13],
-      [10, 14],
-    ],
-    food: [1 ,1],
-    direction: 'down',
-  };
+  currentScore: 0,
+  gameLost: false,
+  shouldRestart: false,
+  pause: false,
+  time: 0,
+  snakeBody: [
+    [10, 10],
+    [10, 11],
+    [10, 12],
+    [10, 13],
+    [10, 14],
+  ],
+  food: [1, 1],
+  direction: "up",
+};
 
 const engine = createEngine();
 
@@ -34,11 +34,10 @@ engine.addSideEffect({
   onlyWhen: ifScoreChanged,
   effect: (_, emit) => {
     emit("createFood");
-    emit("snakeGrowUp");
     const intervalId = setInterval(() => {
       emit("createFood");
     }, 15000);
-    
+
     return () => {
       clearInterval(intervalId);
     };
@@ -46,20 +45,20 @@ engine.addSideEffect({
 });
 
 engine.addSideEffect({
-    onlyWhen: ifDirectionChanged,
-    effect: (_, emit) => {
+  onlyWhen: ifDirectionChanged,
+  effect: (_, emit) => {
+    emit("snakeMove");
+    const intervalId = setInterval(() => {
       emit("snakeMove");
-      const intervalId = setInterval(() => {
-        emit("snakeMove");
-      }, 1000);
-      
-      return () => {
-        clearInterval(intervalId);
-      };
-    },
-  });
-  
-  engine.addSideEffect({
+    }, 185);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  },
+});
+
+engine.addSideEffect({
     onlyWhen: ({ prevState, state }) =>
       getIsStateChanged("time", { prevState, state }),
     effect: (_, emit) => {
@@ -75,312 +74,160 @@ engine.addSideEffect({
   
   engine.addSideEffect({
     onlyWhen: ({ prevState, state }) =>
+      getIsStateChanged("gameLost", { prevState, state }),
+    effect: renderGameAlert,
+  });
+  
+  engine.addSideEffect({
+    onlyWhen: ({ prevState, state }) =>
       getIsStateChanged("currentScore", { prevState, state }),
-    effect: renderGameScore    
+    effect: renderGameScore,
   });
   
   engine.addSideEffect({
     onlyWhen: ({ prevState, state }) =>
       getIsStateChanged("time", { prevState, state }),
-    effect: renderGameTime
+    effect: renderGameTime,
   });
   
   engine.addSideEffect({
     effect: renderGameBoard,
   });
   
-  engine.addGlobalReducer( state  => {
-    if (getIsGameEnded(state)) {
+  engine.addGlobalReducer((state) => {
+    if (getIsGameEnded(state) || getIsGamePause(state)) {
       return state;
-    };
+    }
   
-    if(getIsGamePause(state)) {
-      return state;
-    };
+    const [snakeHeadX, snakeHeadY] = [...state.snakeBody[0]];
   
-    const snakeHead = [...state.snakeBody[0]];
-    const pointForEat = 1;
-    let gameLost = false;
-    let score = 0;  
-    
-    if (state.snakeBody[0][0] === state.food[0] && state.snakeBody[0][1] === state.food[1]) {
-      score = pointForEat;
-    };
+    const snakeHeadOnBody = state.snakeBody.filter(([x, y]) => x === snakeHeadX && y === snakeHeadY);
   
-    const snakeHeadOnBody = state.snakeBody.filter( item => item[0] === snakeHead[0] && item[1] === snakeHead[1] );
-  
-    if(snakeHeadOnBody.length == 2) {
-      gameLost = true;
-    };
+    const gameLost = snakeHeadOnBody.length === 2;
   
     return {
       ...state,
-      currentScore: state.currentScore + score,
       gameLost,
     };
   });
-
-engine.addSignalReducer('nextSecond', state => {
+  
+  
+  addMovement(engine, {signal: "keyClickUp", direction: "up", dontMove: "down"});
+  addMovement(engine, {signal: "keyClickDown", direction: "down", dontMove: "up"});
+  addMovement(engine, {signal: "keyClickLeft", direction: "left", dontMove: "right"});
+  addMovement(engine, {signal: "keyClickRight", direction: "right", dontMove: "left"});
+  
+  engine.addSignalReducer("pauseGame", (state) => {
     if (getIsGameEnded(state)) {
       return state;
-    };
-    if (getIsGamePause(state)) {
-      return state;
-    };
-  
-    const timeSec = 1;
-    const timeWon = 120;
-    let endGameWinning = false;
-  
-    if (state.time === timeWon - 1) {
-        endGameWinning = true;
-    };
-  
-    return ({
-      ...state,
-      time: state.time + timeSec,
-      endGameWinning
-    });
-  });
-
-  engine.addSignalReducer('keyClickDown', state => {
-    if (getIsGameEnded(state)) {
-      return state;
-    };
-    if(getIsGamePause(state)) {
-      return state;
-    };
-  
-    const direction = 'down';
+    }
   
     return {
       ...state,
-      direction,
+      pause: !state.pause,
     };
   });
   
-  engine.addSignalReducer('keyClickUp', state => {
-    if (getIsGameEnded(state)) {
+  engine.addSignalReducer("snakeMove", (state) => {
+    if (getIsGameEnded(state) || getIsGamePause(state)) {
       return state;
-    };
-    if(getIsGamePause(state)) {
-      return state;
+    }
+    const snakeBodyBefore = [...state.snakeBody];
+    const [snakeHeadX, snakeHeadY] = [...state.snakeBody[0]];
+    const [foodX, foodY] = [...state.food];
+    const pointForEat = 1;
+    let snakeBodyFirstElem = [snakeHeadX, snakeHeadY];
+  
+    const movements = {
+      right: ([x, y]) => [x + 1, y],
+      left: ([x, y]) => [x - 1, y],
+      up: ([x, y]) => [x, y - 1],
+      down: ([x, y]) => [x, y + 1],
     };
   
-    const direction = 'up';
+    const moveSnakeHead = movements[state.direction];
+    const snakeBodyFirstElemMove = moveSnakeHead(snakeBodyFirstElem);
+    const gameLost = getIsSnakeHeadOutsideBoard(...snakeBodyFirstElemMove);
+    const snakeBodyWithExtraTail = snakeBodyWithTail(
+      gameLost,
+      snakeBodyFirstElem,
+      snakeBodyFirstElemMove,
+      snakeBodyBefore
+    );
+    const hasEatenFood = getHasEatenFood(snakeBodyFirstElemMove, foodX, foodY);
+    const snakeBody = growSnake(
+      hasEatenFood,
+      snakeBodyBefore,
+      snakeBodyFirstElemMove,
+      snakeBodyWithExtraTail
+    );
+  
     return {
-        ...state,
-        direction,
-     };
+      ...state,
+      snakeBody,
+      gameLost,
+      currentScore: hasEatenFood
+        ? state.currentScore + pointForEat
+        : state.currentScore,
+    };
   });
-    
- engine.addSignalReducer('keyClickLeft', state => {
-     if (getIsGameEnded(state)) {
+  
+  engine.addSignalReducer("createFood", (state) => {
+    if (getIsGameEnded(state) || getIsGamePause(state)) {
       return state;
-     };
-    if(getIsGamePause(state)) {
-      return state;
-    };
-      
-    const direction = 'left';
-      
+    }
+  
+    const snakeBody = [...state.snakeBody];
+  
+    const food = generateNewFood(snakeBody);
+  
     return {
-     ...state,
-     direction,
-     };
-  });
-      
- engine.addSignalReducer('keyClickRight', state => {
-     if (getIsGameEnded(state)) {
-       return state;
-    };
-    if(getIsGamePause(state)) {
-       return state;
-     };
-      
-    const direction = 'right';
-    return {
-        ...state,
-        direction,
-   };
-});
-        
-     engine.addSignalReducer('pauseGame', state =>  {
-        if (getIsGameEnded(state)) {
-         return state;
-         };
-        
-        return {
-          ...state,
-          pause: !state.pause,
-    };
- });
-        
- engine.addSignalReducer('snakeGrowUp', state => {
-        if (getIsGameEnded(state)) {
-          return state;
-            };
-        if(getIsGamePause(state)) {
-          return state;
-            };
-            const snakeBody = [...state.snakeBody, [+state.snakeBody[state.snakeBody.length - 1][0], +state.snakeBody[state.snakeBody.length - 1][1]]];
-            
-        return {
-         ...state,
-        snakeBody,
-    };
- });
-
-engine.addSignalReducer('snakeMove', state => {
-     if (getIsGameEnded(state)) {
-      return state;
-            };
-     if(getIsGamePause(state)) {
-      return state;
-            };
-          
-        let snakeCoordinates = [...state.snakeBody[0]];
-        let snakeBodyFirstElem = [];
-        let endGameLosing = false;
-
-
-        if (state.direction === 'right') {
-        if (snakeCoordinates[0] < 25) {
-          snakeBodyFirstElem = [+snakeCoordinates[0] + 1, snakeCoordinates[1]];
-            } else {
-          snakeBodyFirstElem = [+snakeCoordinates[0], snakeCoordinates[1]];
-             endGameLosing = true;
-            }
-
-            } else if (state.direction === 'left') {
-             if (snakeCoordinates[0] > 1) {
-                  snakeBodyFirstElem = [+snakeCoordinates[0] - 1, snakeCoordinates[1]];
-
-            } else {
-                  snakeBodyFirstElem = [+snakeCoordinates[0], snakeCoordinates[1]];
-                  endGameLosing = true;
-            }
-
-            } else if (state.direction === 'down') {
-                if (snakeCoordinates[1] > 1) {
-                  snakeBodyFirstElem = [snakeCoordinates[0], +snakeCoordinates[1] - 1];
-                } else {
-                  snakeBodyFirstElem = [+snakeCoordinates[0], snakeCoordinates[1]];
-                  endGameLosing = true;
-                }
-
-              } else if (state.direction === 'up') {
-                if (snakeCoordinates[1] < 25) {
-                  snakeBodyFirstElem = [snakeCoordinates[0], +snakeCoordinates[1] + 1];
-
-                } else {
-                  snakeBodyFirstElem = [+snakeCoordinates[0], snakeCoordinates[1]];
-                  endGameLosing = true;
-     }
- };
-
- const snakeBody = [snakeBodyFirstElem, ...state.snakeBody];
- snakeBody.pop();
-            
-   return {
-    ...state,
-    snakeBody,
-     endGameLosing,
-     };
-});
-
- engine.addSignalReducer('createFood', state => {
-  if (getIsGameEnded(state)) {
-     return state;
-  };
-
-  if(getIsGamePause(state)) {
-      return state;
-  };
-                
-     const snakeBody = [...state.snakeBody];
-              
-     let food = [getRandomNumber(25), getRandomNumber(25)];
-     let foodOnSnake = snakeBody.filter( item => item[0] === food[0] && item[1] === food[1] );
-              
-     while(foodOnSnake.length === 1) {
-     food = [getRandomNumber(25), getRandomNumber(25)];
-     foodOnSnake = snakeBody.filter( item => item[0] === food[0] && item[1] === food[1] );
-  };
-              
-      return {
       ...state,
       food,
     };
- });
-
- 
- engine.start();
-
- function renderGameBoard({ state, prevState }, emit) {
-   let meshContainer = document.querySelector(".mesh");
- 
-   if (!meshContainer) {
-     meshContainer = document.createElement("div");
-     meshContainer.classList.add("mesh");
-     let x = 1;
-     let y = 25;
-     for (let i = 0; i < meshSize; i++) {
-         const cell = document.createElement("div");
-         cell.classList.add("item");
-         meshContainer.appendChild(cell);
-         document.body.appendChild(meshContainer);
-     
-         if(x > 25) {
-             x = 1;
-             y--;
-         };
-         let excel = document.querySelectorAll('.item');
-         excel[i].setAttribute('posX', x);
-         excel[i].setAttribute('posY', y);
-         x++;  
-     };
-   };
- 
-   const snakeBody = state.snakeBody.map( item => document.querySelector(`[posX = '${item[0]}'][posY = '${item[1]}']`) );
-   snakeBody.forEach(item => item.classList.add('snakeBody'));
-   snakeBody[0].classList.add('snakeHead');
-
-   const onDocumentClick = (e) => {
-    if (e.key === 'ArrowLeft' && state.direction !== 'right') {
-      emit("keyClickLeft");
-    } else if (e.key === 'ArrowUp' && state.direction !== 'down') {
-      emit("keyClickUp");
-    } else if (e.key === 'ArrowRight' && state.direction !== 'left') {
-      emit("keyClickRight");
-    } else if (e.key === 'ArrowDown' && state.direction !== 'up') {
-      emit("keyClickDown");
-    } else if (e.keyCode == 32) {
-      emit("pauseGame")
+  });
+  
+  engine.start();
+  
+  function renderGameAlert({ state, prevState }, emit) {
+    let alert = document.querySelector(".alert");
+  
+    if (!alert) {
+      alert = document.createElement("div");
+      alert.classList.add("alert");
+      alert.innerHTML = 'The game ended, please press "Enter" to restart';
+      document.body.appendChild(alert);
     }
-  };
-
-  window.addEventListener('keydown', onDocumentClick);
-
-  return () => {
-    window.removeEventListener('keydown', onDocumentClick);
-    snakeBody.forEach(item => item.classList.remove('snakeBody'));
-    snakeBody[0].classList.remove('snakeHead');
-    food.classList.remove('food');
-  };
-};
-
-function renderGameScore({ state, prevState }, emit) {
+  
+    const onAlertEnter = (e) => {
+      if (e.key === "Enter") {
+        emit("restartGame");
+      }
+    };
+  
+    if (state.gameLost) {
+      document.addEventListener("keyup", onAlertEnter);
+      alert.classList.add(state.gameLost, "alert-loss");
+    } else {
+      alert.classList.remove("alert-loss");
+    }
+  
+    return () => {
+      document.removeEventListener("keyup", onAlertEnter);
+    };
+  }
+  
+  function renderGameScore({ state, prevState }, emit) {
     let scoreContainer = document.querySelector(".score");
   
     if (!scoreContainer) {
       scoreContainer = document.createElement("div");
       scoreContainer.classList.add("score");
       document.body.appendChild(scoreContainer);
-    };
+    }
   
     scoreContainer.innerHTML = state.currentScore;
-  };
+  }
   
   function renderGameTime({ state, prevState }, emit) {
     let timeContainer = document.querySelector(".time");
@@ -392,17 +239,167 @@ function renderGameScore({ state, prevState }, emit) {
     }
   
     timeContainer.innerHTML = state.time;
-  };
-
-  function getRandomNumber(max) {
-    return Math.floor(Math.random() * max + 1);
-  };
+  }
+  
+  function renderGameBoard({ state, prevState }, emit) {
+    let meshContainer = document.querySelector(".mesh");
+  
+    if (!meshContainer) {
+      meshContainer = document.createElement("div");
+      meshContainer.classList.add("mesh");
+  
+      for (let i = 0; i < meshSize; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("item");
+        meshContainer.appendChild(cell);
+        document.body.appendChild(meshContainer);
+        const x = i % boardWidth;
+        const y = Math.floor(i / boardWidth);
+  
+        let excel = document.querySelectorAll(".item");
+        excel[i].dataset.x = x;
+        excel[i].dataset.y = y;
+      }
+    }
+  
+    const snakeBody = state.snakeBody.map(([x, y]) => {
+      const meshIndex = x + y * boardWidth;
+      return meshContainer.children[meshIndex];
+    });
+    snakeBody.forEach((item) => item.classList.add("snakeBody"));
+    snakeBody[0].classList.add("snakeHead");
+  
+    const food = document.querySelector(
+      `[data-x = '${state.food[0]}'][data-y = '${state.food[1]}']`
+    );
+    food.classList.add("food");
+  
+    const onDocumentClick = (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          emit("keyClickLeft");
+          break;
+        case "ArrowUp":
+          emit("keyClickUp");
+          break;
+        case "ArrowRight":
+          emit("keyClickRight");
+          break;
+        case "ArrowDown":
+          emit("keyClickDown");
+          break;
+      }
+      if (e.code === "Space") {
+        emit("pauseGame");
+      }
+    };
+  
+    window.addEventListener("keydown", onDocumentClick);
+  
+    return () => {
+      window.removeEventListener("keydown", onDocumentClick);
+      snakeBody.forEach((item) => item.classList.remove("snakeBody"));
+      snakeBody[0].classList.remove("snakeHead");
+      food.classList.remove("food");
+    };
+  }
+  
+  function addMovement(engine, obj) {
+    engine.addSignalReducer(obj.signal, (state) => {
+      if (
+        getIsGameEnded(state) ||
+        getIsGamePause(state) ||
+        state.direction === obj.dontMove
+      ) {
+        return state;
+      }
+  
+      const direction = obj.direction;
+  
+      return {
+        ...state,
+        direction,
+      };
+    });
+  }
+  
+  function getHasEatenFood([snakeHeadX, snakeHeadY], foodX, foodY) {
+    if (snakeHeadX === foodX && snakeHeadY === foodY) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   
   function getIsGameEnded(state) {
-    return state.endGameWinning || state.endGameLosing;
-  };
+    return state.gameLost;
+  }
   
   function getIsGamePause(state) {
     return state.pause;
-  };
- 
+  }
+  
+  function getIsStateChanged(stateKey, payload) {
+    return payload.prevState?.[stateKey] !== payload.state[stateKey];
+  }
+  
+  function ifDirectionChanged({ prevState, state }) {
+    return (
+      getIsStateChanged("direction", { prevState, state }) &&
+      !getIsGameEnded(state)
+    );
+  }
+  
+  function ifScoreChanged({ prevState, state }) {
+    return (
+      getIsStateChanged("currentScore", { prevState, state }) &&
+      !getIsGameEnded(state)
+    );
+  }
+  
+  function getIsSnakeHeadOutsideBoard(headX, headY) {
+    if (headX > 24 || headX < 0 || headY > 24 || headY < 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  function growSnake(hasEatenFood, snakeBody, snakeHead, snakeBodyWithExtraTail) {
+    if (hasEatenFood) {
+      return [snakeHead, ...snakeBody];
+    } else {
+      return snakeBodyWithExtraTail.slice(0, snakeBodyWithExtraTail.length - 1);
+    }
+  }
+  
+  function snakeBodyWithTail(
+    gameLost,
+    snakeBodyFirstElem,
+    snakeBodyFirstElemMove,
+    snakeBody
+  ) {
+    if (gameLost) {
+      return [snakeBodyFirstElem, ...snakeBody];
+    } else {
+      return [snakeBodyFirstElemMove, ...snakeBody];
+    }
+  }
+  
+  function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  function generateNewFood(snakeBody) {
+    let food = [getRandomNumber(25), getRandomNumber(25)];
+    let foodOnSnake = snakeBody.filter(
+      (item) => item[0] === food[0] && item[1] === food[1]
+    );
+  
+    while (foodOnSnake.length === 1) {
+      food = [getRandomNumber(25), getRandomNumber(25)];
+      foodOnSnake = snakeBody.filter(
+        (item) => item[0] === food[0] && item[1] === food[1]
+      );
+    }
+    return food;
+  }
