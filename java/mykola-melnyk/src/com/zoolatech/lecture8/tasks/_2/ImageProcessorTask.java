c9c36c2084ce1307e21aa044aa73c1582c3a8d27
Mykola-Melnyk <mr.nicholas.x@gmail.com>
2022-09-12 16:09:01 +0300

    lecture 8 task 2 testing function  added

--- java/mykola-melnyk/src/com/zoolatech/lecture8/tasks/_2/ImageProcessorTask.java
+++ java/mykola-melnyk/src/com/zoolatech/lecture8/tasks/_2/ImageProcessorTask.java
@@ -1,6 +1,7 @@
 package com.zoolatech.lecture8.tasks._2;
 
 import java.util.concurrent.RecursiveAction;
+import java.util.function.Function;
 
 public class ImageProcessorTask extends RecursiveAction {
     private static final int MAX_SIZE = 10;
@@ -10,18 +11,20 @@ public class ImageProcessorTask extends RecursiveAction {
     private final int toX;
 
     private final int[][] pixels;
+    private final Function<Integer, Integer> f;
 
 
-    public ImageProcessorTask(int fromX, int toX, int fromY, int toY, int[][] pixels) {
+    public ImageProcessorTask(int fromX, int toX, int fromY, int toY, int[][] pixels, Function<Integer, Integer> f) {
         this.fromX = fromX;
         this.toX = toX;
         this.fromY = fromY;
         this.toY = toY;
         this.pixels = pixels;
+        this.f = f;
     }
 
-    public ImageProcessorTask(int[][] pixels) {
-        this(0, pixels.length, 0, pixels.length, pixels);
+    public ImageProcessorTask(int[][] pixels, Function<Integer, Integer> f) {
+        this(0, pixels.length, 0, pixels.length, pixels, f);
     }
 
     @Override
@@ -29,15 +32,15 @@ public class ImageProcessorTask extends RecursiveAction {
         if ((toX - fromX <= MAX_SIZE) || (toY - fromY <= MAX_SIZE)) {
             for (int x = fromX; x < toX; x++) {
                 for (int y = fromY; y < toY; y++)
-                    pixels[x][y] += 1;
+                    pixels[x][y]  = f.apply(pixels[x][y]);
             }
         } else {
             int midX = fromX + (toX - fromX) / 2;
             int midY = fromY + (toY - fromY) / 2;
-            invokeAll(new ImageProcessorTask(fromX, midX, fromY, midY, pixels),
-                    new ImageProcessorTask(fromX, midX, midY, toY, pixels),
-                    new ImageProcessorTask(midX, toX, fromY, midY, pixels),
-                    new ImageProcessorTask(midX, toX, midY, toY, pixels));
+            invokeAll(new ImageProcessorTask(fromX, midX, fromY, midY, pixels, f),
+                    new ImageProcessorTask(fromX, midX, midY, toY, pixels, f),
+                    new ImageProcessorTask(midX, toX, fromY, midY, pixels, f),
+                    new ImageProcessorTask(midX, toX, midY, toY, pixels, f));
         }
     }
 }
--- java/mykola-melnyk/src/com/zoolatech/lecture8/tasks/_2/Task2.java
+++ java/mykola-melnyk/src/com/zoolatech/lecture8/tasks/_2/Task2.java
@@ -1,6 +1,7 @@
 package com.zoolatech.lecture8.tasks._2;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.function.Function;
 
 /**
  * Create a class ImageProcessorTask that accepts an image (represented as an int[][] array, where each item
@@ -17,14 +18,13 @@ public class Task2 {
         int imgSize = 20;
         ForkJoinPool pool = ForkJoinPool.commonPool(); // create a pool of threads
         int[][] pixels = new int[imgSize][imgSize]; // create an array
-        int inc = 1; // fill with increment numbers
         for (int i = 0; i < imgSize; i++) {
             for (int j = 0; j < imgSize; j++)
                 pixels[i][j] = 1;
         }
+        Function<Integer, Integer> f = integer -> integer + 1;  // testing function
 
-
-        ImageProcessorTask myImg = new ImageProcessorTask(pixels);
+        ImageProcessorTask myImg = new ImageProcessorTask(pixels, f);
         pool.invoke(myImg); // start the task
 
         for (int i = 0; i < imgSize; i++) {  // print the array
