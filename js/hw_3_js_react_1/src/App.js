943fd1b8eda23f201db5a581cccad2f462d71028
lilproger21 <nnikolastihomirov@gmail.com>
2022-09-13 21:11:00 +0300

    corrected code

--- js/hw_3_js_react_1/src/App.js
+++ js/hw_3_js_react_1/src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import Board from "./components/Board/Board.jsx";
 
 const App = () => {
-
-
   return (
     <div>
-      <Board />        
+      <Board />
     </div>
   );
 };
