a255c5d6484b17396203b18d84da05ffa55accff
lilproger21 <nnikolastihomirov@gmail.com>
2022-09-23 21:46:25 +0300

    Created chat

--- js/hw_4_http_react/src/App.js
+++ js/hw_4_http_react/src/App.js
@@ -1,36 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Chat } from "./components/Chat/Chat";
 
 function App() {
-  const [draftUserName, setDraftUserName] = React.useState("");
-  const [userName, setUserName] = React.useState("");
+  const [draftUserName, setDraftUserName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userMessage, setUserMessage] = useState("");
+
+  const submitHandler = async () => {
+    if (userName.length === 0 || userMessage.length === 0) {
+      return;
+    }
+
+    const today = new Date();
+    const data = {
+      user: userName,
+      message: userMessage,
+      time: today.toLocaleTimeString("en-US"),
+    };
+
+    await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   if (!userName) {
     return (
-      <form
-        style={{
-          position: "fixed",
-          height: "100vh",
-          width: "100vw",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          setUserName(draftUserName);
-          setDraftUserName("");
-        }}
-      >
-        <label>
-          Enter your username:{" "}
-          <input onChange={(e) => setDraftUserName(e.target.value)} />
-        </label>
-      </form>
+      <div className="container">
+        <form
+          className="formName"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setUserName(draftUserName);
+            setDraftUserName("");
+          }}
+        >
+          <label>
+            Enter your username:{" "}
+            <input
+              value={draftUserName}
+              onChange={(e) => setDraftUserName(e.target.value)}
+            />
+          </label>
+        </form>
+      </div>
     );
   }
 
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="container__message">
+        <Chat userMessage={userMessage} userName={userName} />
+      </div>
+      <form
+        className="formMessage"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setUserMessage("");
+        }}
+      >
+        <input
+          value={userMessage}
+          onChange={(e) => setUserMessage(e.target.value)}
+        />
+        <button onClick={submitHandler}>send</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
