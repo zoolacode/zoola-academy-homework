19f9aba922bb5bca6b27b3b82d3b77da5f8d0c94
Aleksandr Kulyk <ale.kulyk@gmail.com>
2022-10-25 01:05:30 +0300

    create component ChatList

---
+++ js/chat_app/src/services/userServices.js
@@ -0,0 +1,37 @@
+export const userServices = {
+    getAllUsers: async (authToken) => {
+      try {
+        const data = await fetch('/api/users', {
+          method: 'GET',
+          headers: {
+            'content-type': 'application/json',
+            'Auth-Token': authToken
+          }
+        });
+  
+        const response = data.json();
+  
+        return response;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    createUser: async (authToken, username, password, adminId) => {
+      try {
+        await fetch('/api/users', {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'Auth-Token': authToken
+          },
+          body: JSON.stringify({
+            adminId,
+            username,
+            password
+          })
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
