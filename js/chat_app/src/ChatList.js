8848e627190966c904b1335ce08304a10401de09
NicolasKul <nicolaychikk@gmail.com>
2022-10-26 09:42:28 +0300

    Remove hardcode and add var for authToken and useerId

--- js/chat_app/src/ChatList.js
+++ js/chat_app/src/ChatList.js
@@ -1,18 +1,18 @@
 import React from "react";
+import { UserContext } from "./UserContext";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import MenuItem from "@mui/material/MenuItem";
 import MenuList from "@mui/material/MenuList";
 
-//temporary hardcode
-{
-  const userId = "fhs8dhf9s8dhf9sd8hf9sd8hf";
-  const authToken = "c4a6ee4f-b87b-4390-88ae-e068237aad89";
-}
-
 export const ChatList = ({ onSelectChat }) => {
   const [selectedId, setSelectedID] = React.useState(1);
   const [chatsData, setChatsData] = React.useState([]);
+  const { auth } = useContext(UserContext);
+
+  const userId = auth?.user.id;
+  const authToken = auth?.authToken;
+
 
   const handleChatListItemClick = (event, id) => {
     //`GET /api/chats/:chatId`
