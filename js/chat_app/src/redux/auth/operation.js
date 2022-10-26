20f04ba961c7ab50971f0b2c5364413c9cc54bdd
Nikita <nikycherenko@gmail.com>
2022-10-26 20:43:45 +0300

    Merge branch 'chat_app_group_3' into Implement_User_Form

---
+++ js/chat_app/src/redux/auth/operation.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import login from '../../services/authServices';
+
+const loginAuth = createAsyncThunk('auth/login', async ({ username, password }) => {
+  const response = await login({
+    username,
+    password
+  });
+  const data = await response.json();
+  localStorage.setItem(
+    'auth',
+    JSON.stringify({
+      authToken: data.authToken,
+      user: {
+        username: data.user.username
+      }
+    })
+  );
+  return data;
+});
+
+export default loginAuth;
