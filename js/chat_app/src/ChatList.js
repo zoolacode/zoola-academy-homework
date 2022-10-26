17a86d4724212f5c5d82e6be26dad243779a7ad5
NicolasKul <nicolaychikk@gmail.com>
2022-10-26 17:41:13 +0300

    Add state to App.js for future using in chat history

--- js/chat_app/src/ChatList.js
+++ js/chat_app/src/ChatList.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { UserContext } from "./UserContext";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import MenuItem from "@mui/material/MenuItem";
 import MenuList from "@mui/material/MenuList";
 
-export const ChatList = ({ onSelectChat }) => {
-  const [selectedId, setSelectedID] = React.useState(1);
+export const ChatList = ({ setChatID }) => {
   const [chatsData, setChatsData] = React.useState([]);
   const { auth } = useContext(UserContext);
 
@@ -17,18 +16,20 @@ export const ChatList = ({ onSelectChat }) => {
   const handleChatListItemClick = (event, id) => {
     //`GET /api/chats/:chatId`
     // draw chat history use onSelectChat component
-    setSelectedID(id);
+    setChatID(id);
   };
 
-  fetch(`api/users/${userId}/chats`, {
-    method: "GET",
-    headers: {
-      "auth-token": authToken,
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then(setChatsData);
+  React.useEffect(() => {
+    fetch(`api/users/${userId}/chats`, {
+      method: "GET",
+      headers: {
+        "auth-token": authToken,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then(setChatsData);
+  },[])
 
   return (
     <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
