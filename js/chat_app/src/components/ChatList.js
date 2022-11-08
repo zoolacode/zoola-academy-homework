60f0250815fc990abdf1b601d533820e0ea34c00
Mykhailo Miroshnikov <mykhailo.miroshnikov-contractor@procore.com>
2022-11-08 10:16:02 +0200

    fix ChatList

--- js/chat_app/src/components/ChatList.js
+++ js/chat_app/src/components/ChatList.js
@@ -1,46 +1,21 @@
-import React, { useContext } from "react";
-import { UserContext } from "./UserContext";
-import { useHttpClient } from "./serverResponse";
+import React from "react";
 import { Box, Paper, MenuItem, MenuList, Typography } from "@mui/material";
 
 export const ChatList = ({ chatId, setChatID, chats }) => {
-  const [chatsData, setChatsData] = React.useState([]);
-  const { auth } = useContext(UserContext);
-
-  const userId = auth?.user.id;
-  //const token = auth?.authToken;
-
   const handleChatListItemClick = (event, id) => {
     setChatID(id);
   };
-  const fetchJSON = useHttpClient();
-
-  React.useEffect(() => {
-    fetch(`api/users/${userId}/chats`, {
-      method: "GET",
-      headers: {
-        "auth-token": authToken,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then(setChatsData);
-  }, [chats]);
-    const url = `api/users/${userId}/chats`;
-
-    fetchJSON(url).then(setChatsData);
-  }, []);
 
   return (
     <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       <Paper>
-        {chatsData.length === 0 ? (
+        {chats.length === 0 ? (
           <Typography align="center" sx={{ p: 1 }}>
             No existing chats
           </Typography>
         ) : (
           <MenuList>
-            {chatsData.map((chat) => {
+            {chats.map((chat) => {
               return (
                 <MenuItem
                   selected={chatId === chat.id}
